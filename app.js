56e516aba37224aeceb7e1ea610b1f1c452c7bf3
ValentinaMontoya <lorenamontoya2004@gmail.com>
2022-07-18 21:12:17 -0500

    actividad-1

---
+++ app.js
@@ -0,0 +1,8 @@
+let name = 'Valentina',
+    lastname = 'Montoya',
+    yearOfBirth = 2004,
+    cityOfBirth = 'Medellín'
+
+console.log(
+`Hola, mi nombre es ${name} ${lastname}, vivo en ${cityOfBirth} y tengo ${
+        2022 - yearOfBirth} años`)
